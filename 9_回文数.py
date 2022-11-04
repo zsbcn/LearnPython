337a2ebd46f9d690bfcbd56a7d4e64bba2c0527c
zsbcn <inzhang@foxmail.com>
2022-11-05 00:50:27 +0800

    回文数更新两个版本实现方法

--- 9_回文数.py
+++ 9_回文数.py
@@ -12,8 +12,21 @@
 
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        reverse_x = str(x)[::-1]
-        if reverse_x == str(x):
+        """
+        判断回文数一句话版
+        :param x: 输入的数字
+        :return: 返回TRUE是回文数；False不是回文数
+        """
+        return True if str(x)[::-1] == str(x) else False
+
+    def isPalindrome_2(self, x: int) -> bool:
+        """
+        判断回文数正常版
+        :param x: 输入的数字
+        :return: 返回TRUE是回文数；False不是回文数
+        """
+        reverse_x = str(x)[::-1]    # 输入x转字符串后倒序
+        if reverse_x == str(x):     # 判断倒序的字符串和正序字符串是否相等？相等：是回文数，否则不是
             return True
         else:
             return False
