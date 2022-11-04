a6a1e64926b04099a4779ba22669b81e13828170
zsbcn <inzhang@foxmail.com>
2022-11-04 22:50:37 +0800

    力扣2169题

---
+++ 2169_得到0的操作数.py
@@ -0,0 +1,10 @@
+# !/usr/bin/env python
+# -*-coding:utf-8 -*-
+
+"""
+# File       : 2169_得到0的操作数.py
+# Time       ：2022-11-4 22:48
+# Author     ：zsbcn
+# version    ：python 3.10
+# Description：力扣第2169题
+"""
