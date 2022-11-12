a2e529bd3f6ea7be69494221b902ff7298086136
zsbcn <inzhang@foxmail.com>
2022-11-12 22:53:17 +0800

    第35题 搜索插入位置

---
+++ 58_最后一个单词的长度.py
@@ -0,0 +1,19 @@
+# !/usr/bin/env python
+# -*-coding:utf-8 -*-
+
+"""
+# File       : 58_最后一个单词的长度.py
+# Time       ：2022-11-12 22:51
+# Author     ：zsbcn
+# version    ：python 3.10
+# Description：
+"""
+
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        pass
+
+
+if __name__ == '__main__':
+    pass
